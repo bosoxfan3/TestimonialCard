9d30e68ce6e285f5f4274f51354fbdb429e1bc6c
Daniel Acquesta <dgacquesta@gmail.com>
2025-03-13 15:04:31 -0700

    removed image styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ type Props = {
 const Header = ({ pictureUrl, name, username }: Props) => {
     return (
         <div className="flex gap-4">
-            <img className="rounded-full" src={pictureUrl} alt={name} width="48px" height="48px" />
+            <img src={pictureUrl} alt={name} width="48px" height="48px" />
             <div>
                 <div className="text-lg font-medium">{name}</div>
                 <div className="text-sm text-neutral-600">@{username}</div>
