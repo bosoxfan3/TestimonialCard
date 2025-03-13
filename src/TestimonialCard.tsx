51ebe3a0f95bd06d33f59b21d3d90a26be73bf12
Daniel Acquesta <dgacquesta@gmail.com>
2025-03-13 14:28:40 -0700

    use hardcoded photo url

--- src/TestimonialCard.tsx
+++ src/TestimonialCard.tsx
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import Info from './components/Info';
 
-const logo = require('./profile-thumbnail.png');
-
 const TestimonialCard = () => {
     return (
         <div className="bg-white p-6 rounded-lg mt-[200px] w-[340px] mx-auto flex flex-col gap-4">
-            <Header pictureUrl={String(logo)} name="Sarah Dole" username="sarahdole" />
+            <Header
+                pictureUrl="https://vaqybtnqyonvlwtskzmv.supabase.co/storage/v1/object/public/projects-images/testimonial-card/starter/img/profile-thumbnail.jpg"
+                name="Sarah Dole"
+                username="sarahdole"
+            />
             <Info infoText="I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!" />
         </div>
     );
