9515f7c2140c3ebefe02f790f3ba6d8d86af9c30
Daniel Acquesta <dgacquesta@gmail.com>
2025-03-13 15:08:48 -0700

    deploying good version

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    content: [
-        './src/**/*.{js,jsx,ts,tsx}', // Note the addition of the `src` directory.
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
