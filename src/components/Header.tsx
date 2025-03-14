2f77af1248ab43332b7e13351b0e07fcfeb580c7
Daniel Acquesta <dgacquesta@gmail.com>
2025-03-14 14:13:02 -0700

    changed font size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = ({ pictureUrl, name, username }: Props) => {
         <div className="flex gap-4">
             <img className="rounded-full" src={pictureUrl} alt={name} width="48px" height="48px" />
             <div>
-                <div className="text-lg font-medium">{name}</div>
+                <div className="text-lg font-semibold">{name}</div>
                 <div className="text-sm text-neutral-600">@{username}</div>
             </div>
         </div>
